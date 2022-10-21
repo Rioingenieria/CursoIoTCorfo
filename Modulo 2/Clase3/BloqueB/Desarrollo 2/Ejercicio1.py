b98331696c14ce059e6a8bff5d24dcc9d5670f88
brykyma <hector.meza@outlook.cl>
2022-10-21 19:46:26 -0300

    Update Ejercicio1.py

--- Modulo 2/Clase3/BloqueB/Desarrollo 2/Ejercicio1.py
+++ Modulo 2/Clase3/BloqueB/Desarrollo 2/Ejercicio1.py
@@ -1,1 +1,10 @@
+def cadena_adn(n):
+    from random import choice
 
+    adn = ""
+    for i in range(n):
+        adn += choice('atcg')
+    return adn
+
+dato=int(input("Ingrese largo: "))
+print(cadena_adn(dato))

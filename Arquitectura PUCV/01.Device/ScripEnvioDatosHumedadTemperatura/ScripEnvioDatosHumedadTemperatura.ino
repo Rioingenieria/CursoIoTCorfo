28587804c9dd7b453b640a2ead6434a654a16d52
Rioingenieria <57739859+Rioingenieria@users.noreply.github.com>
2022-12-21 19:41:17 -0300

    Se crea estructura de carpetas

---
+++ Arquitectura PUCV/01.Device/ScripEnvioDatosHumedadTemperatura/ScripEnvioDatosHumedadTemperatura.ino
@@ -0,0 +1,137 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <LiquidCrystal_I2C.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+
+#define LED             D4
+#define ONE_WIRE_PIN    D6
+#define MSG_BUFFER_SIZE 250
+
+LiquidCrystal_I2C lcd( 0x27, 16, 2 );
+
+OneWire oneWire( D7 );
+DallasTemperature sensor( &oneWire );
+
+// Actualizar parámetros
+const char* ssid = "Rio Ingenieria";
+const char* password = "Rio20199!";
+const char* mqtt_server = "test.mosquitto.org";
+const int mqtt_port = 1883;
+const char* mqtt_topic = "pucv/iot/m6/p3/g40";
+const char* device_id = "id154";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+unsigned long lastMsg = 0;
+int counter = 0;
+
+// función que conecta a WiFi
+void setup_wifi() {
+
+  delay(10);
+  
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  lcd.setCursor( 0, 0 );
+  lcd.print("Connecting to ");
+  lcd.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  lcd.setCursor( 0, 1 );
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    lcd.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  lcd.setCursor( 0, 0 );
+  lcd.print( "IP:" );
+  lcd.print(WiFi.localIP());
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "WemosClient-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  // inicializar el módulo LCD
+  lcd.init();
+  lcd.setBacklight(1);
+
+  // inicializar sensor Dallas
+  sensor.begin();
+
+  // inicializar Wemos
+  pinMode(LED, OUTPUT);
+  Serial.begin(115200);
+
+  // inicializar y conectar a WiFi
+  setup_wifi();
+  
+  // inicializar el generador de números aleatorios
+  randomSeed(micros());
+  
+  client.setServer(mqtt_server, mqtt_port);
+}
+
+void loop() {
+  sensor.requestTemperatures();
+  float temperature = sensor.getTempCByIndex( 0 );
+
+  lcd.setCursor( 0, 1 );
+  lcd.print( temperature );
+  lcd.print(" C");
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  unsigned long now = millis();
+  if (now - lastMsg > 15000) {
+    lastMsg = now;
+    ++counter;
+    
+    DynamicJsonDocument doc(1024);
+    doc["device_id"] = device_id;
+    doc["counter"]   = counter;
+    doc["temperature"] = temperature;
+    doc["version"] = "1.0beta";
+    
+    char msg[MSG_BUFFER_SIZE];
+    serializeJson(doc, msg);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    
+    // publicar el mensaje en JSON serializado
+    client.publish(mqtt_topic, msg);
+  }
+}
